c957fcc5de28f324b3f8ce77a9645fcd86c62c10
Marco Maggi <marco.maggi-ipsu@poste.it>
2011-11-23 17:53:27 +0100

    updated meta files

--- prepare.sh
+++ prepare.sh
@@ -10,6 +10,7 @@ prefix=/usr/local
 LIBFFI_INCLUDEDIR=${prefix}/lib/libffi-3.0.8/include
 
 ../configure \
+    --config-cache                              \
     --prefix="${prefix}"                        \
     --enable-libffi                             \
     CFLAGS='-O3 -march=i686 -mtune=i686'        \
