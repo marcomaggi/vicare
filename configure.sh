7e6844da53dfb26041a16991adb00f6cb9f926d5
Marco Maggi <marco.maggi-ipsu@poste.it>
2012-02-07 15:59:45 +0100

    added configuration option --with-pthread

--- configure.sh
+++ configure.sh
@@ -16,7 +16,8 @@ LIBFFI_INCLUDEDIR=${prefix}/lib/libffi-3.0.10/include
     --with-cre2                                 \
     CFLAGS='-O3 -march=i686 -mtune=i686'        \
     CPPFLAGS="-I${LIBFFI_INCLUDEDIR}"           \
-    LDFLAGS='-L/usr/local/lib -lpthread'        \
     "$@"
+
+## LDFLAGS='-L/usr/local/lib -lpthread'
 
 ### end of file
--- prepare.sh
+++ prepare.sh
@@ -19,7 +19,8 @@ LIBFFI_INCLUDEDIR=${prefix}/lib/libffi-3.0.10/include
     --with-cre2                                 \
     CFLAGS='-g -O3 -march=i686 -mtune=i686'     \
     CPPFLAGS="-I${LIBFFI_INCLUDEDIR}"           \
-    LDFLAGS='-L/usr/local/lib -lpthread'        \
     "$@"
+
+## LDFLAGS='-L/usr/local/lib'
 
 ### end of file
