cf0eb4458418f234585b118b50e9cf876125fce4
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-03-27 11:50:43 +0100

    changed maintainer configuration script to include debugging symbols in C code

--- configure.sh
+++ configure.sh
@@ -15,7 +15,7 @@ LIBFFI_INCLUDEDIR=${prefix}/lib/libffi-3.0.11/include
     --enable-time-tests                         \
     --with-pthread                              \
     --with-cre2                                 \
-    CFLAGS='-O3 -march=i686 -mtune=i686'        \
+    CFLAGS='-g -O3 -march=i686 -mtune=i686'     \
     CPPFLAGS="-I${LIBFFI_INCLUDEDIR}"           \
     "$@"
 
