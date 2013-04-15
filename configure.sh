663aab8bcc3d008dde515347a6ec50f53eef8272
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-04-15 07:26:16 +0200

    added VFLAGS setting in the maintainer configuration script

--- configure.sh
+++ configure.sh
@@ -17,6 +17,7 @@ LIBFFI_INCLUDEDIR=${prefix}/lib/libffi-3.0.11/include
     --with-cre2                                 \
     CFLAGS='-g -O3 -march=i686 -mtune=i686'     \
     CPPFLAGS="-I${LIBFFI_INCLUDEDIR}"           \
+    VFLAGS='-O3'                                \
     "$@"
 
 ## LDFLAGS='-L/usr/local/lib -lpthread'
