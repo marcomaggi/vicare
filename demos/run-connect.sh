59a783f25fd2b6183c3325d0fbf783af4988b8d6
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-05-10 09:11:39 +0200

    started connect.sps demo program

---
+++ demos/run-connect.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Run the CONNECT demo program.
+
+DIRECTORY=$(dirname $(realpath $0))
+EXECUTABLE=$DIRECTORY/connect.sps
+
+exec "$EXECUTABLE" \
+    --debug                                             \
+    $VFLAGS                                             \
+    --                                                  \
+    --verbose                                           \
+    --log-file ~/var/log/vicare-connect.log             \
+    --interface localhost                               \
+    --port 25                                           \
+    "$@"
+
+### end of file
