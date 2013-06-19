a1bdb7eaf4f8026a07db16b52a3bde7b1f8e4966
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-06-19 09:14:28 +0200

    changed admin configuration script for 64-bit

--- configure.sh
+++ configure.sh
@@ -7,17 +7,18 @@ prefix=/usr/local
 LIBFFI_INCLUDEDIR=${prefix}/lib/libffi-3.0.13/include
 
 ../configure \
-    --enable-maintainer-mode                    \
-    --config-cache                              \
-    --cache-file=../config.cache                \
-    --prefix="${prefix}"                        \
-    --enable-binfmt                             \
-    --enable-time-tests                         \
-    --with-pthread                              \
-    --with-cre2                                 \
-    CFLAGS='-O3'				\
-    CPPFLAGS="-I${LIBFFI_INCLUDEDIR}"           \
-    VFLAGS='-O3'                                \
+    --enable-maintainer-mode				\
+    --config-cache					\
+    --cache-file=../config.cache			\
+    --prefix="${prefix}"				\
+    --enable-binfmt					\
+    --enable-time-tests					\
+    --with-pthread					\
+    --with-cre2						\
+    CFLAGS='-O3'					\
+    CPPFLAGS="-I${LIBFFI_INCLUDEDIR}"			\
+    LDFLAGS='-L/usr/local/lib -L/usr/local/lib64'	\
+    VFLAGS='-O3'					\
     "$@"
 
 ## LDFLAGS='-L/usr/local/lib -lpthread'
