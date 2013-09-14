06f3fb008d0862a6f437613ea5287c9b920f08e9
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-09-14 13:04:24 +0200

    removed path setting from autogen script

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,6 @@
 #
 # Run this in the top source directory to rebuild the infrastructure.
 
-export PATH=/bin:/usr/local/bin:/usr/bin:$PATH
-
 set -xe
 test -d autotools		|| mkdir autotools
 test -f autotools/libtool.m4	|| libtoolize
