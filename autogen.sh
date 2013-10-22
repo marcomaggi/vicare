5a737691c7a6d1477b938bebeab59e5f064ab60c
Eric Holk <eric.holk@gmail.com>
2013-10-22 14:29:55 -0400

    autogen.sh now respects the LIBTOOLIZE environment variable.

--- autogen.sh
+++ autogen.sh
@@ -2,9 +2,13 @@
 #
 # Run this in the top source directory to rebuild the infrastructure.
 
+export PATH=/bin:/usr/local/bin:/usr/bin:$PATH
+
+LIBTOOLIZE=${LIBTOOLIZE:=libtoolize}
+
 set -xe
 test -d autotools		|| mkdir autotools
-test -f autotools/libtool.m4	|| libtoolize
+test -f autotools/libtool.m4	|| ${LIBTOOLIZE}
 autoreconf --warnings=all --install --verbose "$@"
 
 ### end of file
