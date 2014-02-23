521967947edb33a4b710d3786077d79f1a1d22c7
Marco Maggi <marco.maggi-ipsu@poste.it>
2014-02-23 20:09:16 +0100

    removed descriptive labels option from maintainer's configure scripts

--- configure32.sh
+++ configure32.sh
@@ -21,7 +21,7 @@ LIBFFI_INCLUDEDIR=${prefix}/lib/libffi-3.0.13/include
     CFLAGS='-m32 -O3 -pedantic'				\
     CPPFLAGS=""						\
     LDFLAGS='-m32'					\
-    VFLAGS='-O3 --descriptive-labels'			\
+    VFLAGS='-O3'					\
     "$@"
 
 ## LDFLAGS='-L/usr/local/lib -lpthread'
