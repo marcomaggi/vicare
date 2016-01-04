e42b34a636c7c78fe45ef93a0424f958a47ce8b7
Marco Maggi <marco.maggi-ipsu@poste.it>
2016-01-04 10:18:59 +0100

    more tables for typed core primitives

--- scheme/makefile.typed-core-primitives.transcoders.scm
+++ scheme/makefile.typed-core-primitives.transcoders.scm
@@ -7,7 +7,7 @@
 ;;
 ;;
 ;;
-;;Copyright (C) 2015 Marco Maggi <marco.maggi-ipsu@poste.it>
+;;Copyright (C) 2015, 2016 Marco Maggi <marco.maggi-ipsu@poste.it>
 ;;
 ;;This program is free  software: you can redistribute it and/or  modify it under the
 ;;terms  of  the  GNU General  Public  License  as  published  by the  Free  Software
@@ -22,39 +22,86 @@
 ;;
 
 
-;;;; core syntactic binding descriptors, typed core primitives: transcoders
+;;;; transcoders, safe primitives
 
 (section
+
+;;; predicates
+
+(declare-type-predicate transcoder? <transcoder>)
+
+;;; --------------------------------------------------------------------
+;;; constructors
 
 (declare-core-primitive make-transcoder
     (safe)
   (signatures
-   ((<symbol> <symbol> <symbol>)	=> (<transcoder>))
+   ((<symbol>)				=> (<transcoder>))
    ((<symbol> <symbol>)			=> (<transcoder>))
-   ((<symbol>)				=> (<transcoder>))))
+   ((<symbol> <symbol> <symbol>)	=> (<transcoder>)))
+  (attributes
+   ;;Not foldable because transcoders are not representable in FASL files.
+   ((_)			effect-free result-true)
+   ((_ _)		effect-free result-true)
+   ((_ _ _)		effect-free result-true)))
 
-(declare-type-predicate transcoder? <transcoder>)
+(declare-parameter native-transcoder	<transcoder>)
 
-(declare-core-primitive transcoder-codec
-    (safe)
-  (signatures
-   ((<transcoder>)			=> (<symbol>))))
+;;; --------------------------------------------------------------------
+;;; accessors
 
-(declare-core-primitive transcoder-eol-style
-    (safe)
-  (signatures
-   ((<transcoder>)			=> (<symbol>))))
+(let-syntax
+    ((declare-transcoder-accessor
+      (syntax-rules ()
+	((_ ?who)
+	 (declare-core-primitive ?who
+	     (safe)
+	   (signatures
+	    ((<transcoder>)	=> (<symbol>)))
+	   (attributes
+	    ((_)		foldable effect-free result-true))))
+	)))
+  (declare-transcoder-accessor transcoder-codec)
+  (declare-transcoder-accessor transcoder-eol-style)
+  (declare-transcoder-accessor transcoder-error-handling-mode)
+  #| end of LET-SYNTAX |# )
 
-(declare-core-primitive transcoder-error-handling-mode
-    (safe)
-  (signatures
-   ((<transcoder>)			=> (<symbol>))))
+;;; --------------------------------------------------------------------
+;;; codec values
 
-(declare-parameter native-transcoder <transcoder>)
+(declare-object-retriever latin-1-codec		foldable	<symbol>)
+(declare-object-retriever utf-8-codec		foldable	<symbol>)
+(declare-object-retriever utf-16-codec		foldable	<symbol>)
+(declare-object-retriever utf-16le-codec	foldable	<symbol>)
+(declare-object-retriever utf-16be-codec	foldable	<symbol>)
+(declare-object-retriever utf-16n-codec		foldable	<symbol>)
+(declare-object-retriever utf-bom-codec		foldable	<symbol>)
+
+(declare-object-retriever native-eol-style	foldable	<symbol>)
+(declare-object-retriever native-endianness	foldable	<symbol>)
 
 /section)
 
+
+;;;; transcoders, unsafe primitives
 
+(section
+
+(declare-core-primitive $data->transcoder
+    (unsafe)
+  (signatures
+   ((<fixnum>)			=> (<transcoder>)))
+  (attributes
+   ((_)				effect-free result-true)))
+
+(declare-core-primitive $transcoder->data
+    (unsafe)
+  (signatures
+   ((<transcoder>)		=> (<fixnum>)))
+  (attributes
+   ((_)				effect-free result-true)))
+
+/section)
 
 ;;; end of file
 ;; Local Variables:
