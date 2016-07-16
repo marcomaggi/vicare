d142d6bac4799f31019e22d6e05510e0b331a2bd
Marco Maggi <marco.maggi-ipsu@poste.it>
2016-07-16 13:47:35 +0200

    review of maintainer's script

--- configure32.sh
+++ configure32.sh
@@ -5,8 +5,8 @@ set -xe
 
 prefix=/usr/local
 libdir=${prefix}/lib
-LIBFFI_VERSION=3.2.1
-LIBFFI_INCLUDEDIR=${libdir}/libffi-${LIBFFI_VERSION}/include
+# LIBFFI_VERSION=3.2.1
+# LIBFFI_INCLUDEDIR=${libdir}/libffi-${LIBFFI_VERSION}/include
 
 # On  Slackware 64-bit  with  Alien's compat32  packages installed:  the
 # source command below will setup the environment needed to build 32-bit
@@ -18,12 +18,15 @@ LIBFFI_INCLUDEDIR=${libdir}/libffi-${LIBFFI_VERSION}/include
 # plain "configure" with maintainer mode enabled.
 
 ../configure \
-    --enable-maintainer-mode							\
-    --prefix="${prefix}"							\
-    --with-pthread								\
-    CFLAGS='-m32 -O3 -pedantic'							\
-    CPPFLAGS=""									\
-    LDFLAGS='-m32'								\
+    --enable-maintainer-mode				\
+    --config-cache					\
+    --cache-file=./config.cache				\
+    --prefix="${prefix}"				\
+    --libdir="${libdir}"				\
+    --with-pthread					\
+    CFLAGS='-m32 -O3 -pedantic'				\
+    CPPFLAGS=""						\
+    LDFLAGS='-m32'					\
     "$@"
 
 ### end of file
